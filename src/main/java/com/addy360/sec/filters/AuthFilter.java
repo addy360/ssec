d782b1d2764de03573756eddfee061a7dee10095
addy360 <abbyboy360@gmail.com>
2022-06-01 19:21:28 +0300

    generating access token and refresh token

--- src/main/java/com/addy360/sec/filters/AuthFilter.java
+++ src/main/java/com/addy360/sec/filters/AuthFilter.java
@@ -1,10 +1,14 @@
 package com.addy360.sec.filters;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -14,13 +18,17 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 
 @Slf4j
 public class AuthFilter extends UsernamePasswordAuthenticationFilter {
     AuthenticationManager authenticationManager;
 
-    public AuthFilter(AuthenticationManager authenticationManager){
+    public AuthFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
     }
 
@@ -33,7 +41,28 @@ public class AuthFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        log.info("Auth : {}",authResult.toString());
+        User user = (User) authResult.getPrincipal();
+        Algorithm algorithm = Algorithm.HMAC256("secret");
+        Date expiresAt = new Date(System.currentTimeMillis() + Duration.ofHours(24).toMillis());
+        String token = JWT.create()
+                .withIssuedAt(new Date())
+                .withExpiresAt(expiresAt)
+                .withSubject(user.getUsername())
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .sign(algorithm);
+
+        String refreshToken = JWT.create()
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + Duration.ofDays(365).toMillis()))
+                .withSubject(user.getUsername())
+                .withIssuer(request.getRequestURL().toString())
+                .sign(algorithm);
+
+        response.setHeader("access-token", token);
+        response.setHeader("refresh-token", refreshToken);
+
+
         super.successfulAuthentication(request, response, chain, authResult);
     }
 
